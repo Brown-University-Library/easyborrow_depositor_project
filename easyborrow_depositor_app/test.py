6641443f918e8cbfd382007fef0da70ca783198e
birkin <birkin.diana@gmail.com>
2021-06-22 17:52:30 -0400

    adds tests.

---
+++ easyborrow_depositor_app/test.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import json, pprint
+
+from django.test import TestCase
+from easyborrow_depositor_app.lib import version_helper
+
+
+# log = logging.getLogger(__name__)
+TestCase.maxDiff = None
+
+
+class VersionUrlTest( TestCase ):
+    """ Checks version url. """
+
+    def test_version_url(self):
+        """ Checks '/version/'. """
+        response = self.client.get( '/version/' )  # project root part of url is assumed
+        self.assertEqual( 200, response.status_code )
+        jsn_dct = json.loads( response.content )
+        self.assertEqual(  dict, type(jsn_dct) )
+        dct_keys = sorted( jsn_dct.keys() )
+        self.assertEqual( ['request', 'response'], dct_keys )
+        response_keys = sorted( jsn_dct['response'].keys() )
+        self.assertEqual( ['timetaken', 'version'], response_keys )
