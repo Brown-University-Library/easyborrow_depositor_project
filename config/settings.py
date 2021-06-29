9f5a4d562ce0ba89c9ae82e920a5854c875f165d
birkin <birkin.diana@gmail.com>
2021-06-29 13:52:59 -0400

    adds static settings.

--- config/settings.py
+++ config/settings.py
@@ -137,7 +137,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+
+STATIC_URL = os.environ['EZB_DEP__STATIC_URL']
+STATIC_ROOT = os.environ['EZB_DEP__STATIC_ROOT']  # needed for collectstatic command
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
