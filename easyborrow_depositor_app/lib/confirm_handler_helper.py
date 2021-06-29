f7fe485e3973c1c18941db4fa1186c2588ae6c15
birkin <birkin.diana@gmail.com>
2021-06-29 08:27:31 -0400

    saves oclcnum.

--- easyborrow_depositor_app/lib/confirm_handler_helper.py
+++ easyborrow_depositor_app/lib/confirm_handler_helper.py
@@ -30,10 +30,15 @@ class ConfHndlrHlpr():
     def save_request_to_ezb_db( self ):
         try:
             ezb_db = RequestLegacyEntry()
+            ## item data --------------------------
             item_dct = json.loads( self.req_data_obj.item_json )
             ezb_db.title = item_dct.get( 'title', '' )
             ezb_db.isbn = self.process_isbn( item_dct )
+            ezb_db.wc_accession = self.process_oclcnum( item_dct )
+
             ezb_db.sfxurl = self.req_data_obj.perceived_url
+            ## patron data --------------------------
+            ## other data --------------------------
             ezb_db.created = datetime.datetime.now()
             ezb_db.save( using='ezborrow_legacy' )
             log.debug( f'ezb_db.id, ``{ezb_db.id}``' )
@@ -62,5 +67,23 @@ class ConfHndlrHlpr():
                     break
         log.debug( f'isbn, ``{isbn}``' )
         return isbn
+
+    def process_oclcnum( self, item_dct ):
+        log.debug( f'item_dct, ``{pprint.pformat(item_dct)}``' )
+        oclc = ''
+        identifiers = item_dct.get('identifier', [])
+        log.debug( f'identifiers, ``{identifiers}``' )
+        for iden in identifiers:
+            log.debug( f'iden, ``{iden}``' )
+            assert type(iden) == dict
+            assert sorted( iden.keys() ) == ['id', 'type']
+            if iden['type'] == 'oclc':
+                log.debug( 'type legit' )
+                if len( iden['id'].strip() ) > 1:
+                    log.debug( 'len id legit' )
+                    oclc = iden['id'].replace( '-', '' )
+                    break
+        log.debug( f'oclc, ``{oclc}``' )
+        return oclc
 
     ## end class ConfHndlrHlpr()
