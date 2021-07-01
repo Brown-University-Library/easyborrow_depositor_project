83461680bbce682805c7267d7bd1b9ae565afac4
birkin <birkin.diana@gmail.com>
2021-07-01 15:51:31 -0400

    enables passenger to load envars.

--- config/wsgi.py
+++ config/wsgi.py
@@ -7,16 +7,37 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
+## original (works for runserver)
+# import os, sys
+
+# from django.core.wsgi import get_wsgi_application
+
+
+# PROJECT_DIR_PATH = os.path.dirname( os.path.dirname(os.path.abspath(__file__)) )
+# sys.path.append( PROJECT_DIR_PATH )
+
+# os.environ[u'DJANGO_SETTINGS_MODULE'] = 'config.settings'  # so django can access its settings
+
+# application = get_wsgi_application()
+
+
+
 import os, sys
 
+import shellvars
 from django.core.wsgi import get_wsgi_application
 
 
 PROJECT_DIR_PATH = os.path.dirname( os.path.dirname(os.path.abspath(__file__)) )
+ENV_SETTINGS_FILE_PATH = os.environ['EZB_DEP__ENV_SETTINGS_PATH']  # set in `httpd/passenger.conf`, and `env/bin/activate`
+
 sys.path.append( PROJECT_DIR_PATH )
 
 os.environ[u'DJANGO_SETTINGS_MODULE'] = 'config.settings'  # so django can access its settings
 
-
+## load up env vars
+var_dct = shellvars.get_vars( ENV_SETTINGS_FILE_PATH )
+for ( key, val ) in var_dct.items():
+    os.environ[key] = val
 
 application = get_wsgi_application()
